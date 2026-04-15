2d26f4ca7177c69e38ffad2baccf937b21d2add6
adyelmoro <iamayyad@gmail.com>
2026-04-15 13:37:48 +0200

    Update onboard.sh with existance check, logging, and proper logic flow

---
+++ onboard.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#1. Ask the admin for the new username
+echo "Enter the name of the new Technician:"
+read NEW_USER
+
+# 1.1. The Check (The 'If' statement goes here)
+if id "$NEW_USER" &>/dev/null; then
+    # This part runs if the user IS found
+    echo "⚠️  Hold on! The user '$NEW_USER' already exists in the system."
+    echo "Aborting setup to prevent duplicate errors."
+else
+    # This part runs only if the user IS NOT found
+    echo "Creating account for $NEW_USER..."
+    sudo adduser --gecos "" $NEW_USER
+    
+    echo "Adding $NEW_USER to va_techs group..."
+    sudo usermod -aG va_techs $NEW_USER
+    
+    echo "✅ Success! $NEW_USER is now a member of the Tech Team."
+	echo "$(date): Admin created user $NEW_USER" >> onboarding_history.log
+	echo "Account setup complete for $NEW_USER"
+    groups $NEW_USER
+fi
